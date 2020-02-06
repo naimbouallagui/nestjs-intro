bae959dad68e72aa6f8259eece4b809a69b000d5
Naim <naim.bouallagui.8@gmail.com>
2020-02-06 14:35:17 +0100

    create a CRUD api's

---
+++ src/products/products.controller.ts
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Controller, Post, Body, Get, Param} from "@nestjs/common";
+import { ProductsService } from "./products.service";
+
+@Controller('products')
+export class ProductsController {
+    constructor(private readonly productsService: ProductsService) { }
+    @Post()
+    async addProduct(
+        // @Body() completeBody: {title: string, description: string, price: number}
+        @Body('title') prodTitle: string,
+        @Body('description') prodDesc: string,
+        @Body('price') prodPrice: number
+    ) {
+        const generatedId = await this.productsService.insertProduct(
+            prodTitle,
+            prodDesc,
+            prodPrice
+        );
+        return { id: generatedId };
+    }
+
+    @Get()
+    async getAllProducts() {
+        const products = await this.productsService.getProducts();
+        return products;
+    }
+    @Get(':id')
+    async getProduct(@Param('id') prodId: string) {
+        return await this.productsService.getSingleProduct(prodId);
+    }
+
+    @Post('update/:id')
+    async updateProduct(
+        @Param('id') prodId: string, 
+        @Body('title') prodTitle: string, 
+        @Body('description') prodDesc: string, 
+        @Body('price') prodPrice: number
+        ) {
+            await this.productsService.updateProduct(prodId,prodTitle,prodDesc,prodPrice);
+            return null;
+    }
+    @Post(':id')
+    async removeProduct(@Param('id') prodId: string) {
+        await this.productsService.deleteProduct(prodId);
+        return null;
+    }
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,75 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { Product } from "./product.model";
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class ProductsService {
+    private products: Product[] = [];
+
+    constructor(@InjectModel('Product') private readonly productModel: Model<Product>) { }
+    async insertProduct(title: string, desc: string, price: number) {
+        const newProduct = new this.productModel({
+            title,
+            description: desc,
+            price,
+        });
+        // newProduct.save().then();
+        const result = await newProduct.save();
+        
+        return result.id as string;
+    }
+
+    async getProducts() {
+        const products = await this.productModel.find().exec();
+        
+        return products.map(prod => ({
+            id: prod.id,
+            title: prod.title,
+            description: prod.description,
+            price: prod.price
+        }));
+        // return this.products.slice();
+    }
+    async getSingleProduct(productId: string) {
+        const product = await this.findProduct(productId);
+        return {
+            id: product.id, 
+            title: product.title, 
+            description: product.description, 
+            price: product.price};
+    }
+
+    async updateProduct(productId: string, title: string, desc: string, price: number) {
+        const updatedProduct = await this.findProduct(productId);
+        if (title) {
+            updatedProduct.title = title;
+        }
+        if (desc) {
+            updatedProduct.description = desc;
+        }
+        if (price) {
+            updatedProduct.price = price;
+        }
+        updatedProduct.save();
+    }
+    async deleteProduct(prodId: string) {
+        // const [product, index] = this.findProduct(prodId);
+        // const index = this.findProduct(prodId)[1];
+        // return this.products.splice(index, 1);
+        await this.productModel.deleteOne({_id: prodId}).exec();
+    }
+    private async findProduct(id: string): Promise<Product> {
+        let product;
+        try{
+            product = await this.productModel.findById(id);
+        }catch (error){
+            throw new NotFoundException('Could not found the product.');
+        }
+        if (!product) {
+            throw new NotFoundException('Could not found the product.');
+        }
+        return product;
+    }
+
+}
